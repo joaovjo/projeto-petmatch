8299ef923a5f111e47cf20ac9106cb7187fe030c
prikasouzadev <prikasouza.dev@gmail.com>
2026-04-02 14:18:40 -0300

    feat: create footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <main>
       <About />
       <Contact />
+      <Footer />
     </main>
   );
 }
