52a0c44447a8052441d97c00551b20971ec9cbb2
Athos13 <athosmachadomuniz@gmail.com>
2026-04-04 18:01:38 -0300

    feat: adiciona componentes CardPet e PreviewPetList

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
+import PetCard from './components/PetCard'
+import PreviewPetList from './components/PreviewPetList'
 
 function App() {
 
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      <h1 className="text-white text-4xl">
-        Projeto funcionando 🚀
-      </h1>
-    </div>
+    <main>
+      <PreviewPetList/>
+    </main>
   )
 }
 
